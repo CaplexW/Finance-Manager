a902a0ee8a31ff0641baf8baa1a907bdf95106b4
caplex <caplex.work@email.com>
2025-04-23 12:12:56 +0700

    backup save

--- src/client/app/components/UI/NavBar.jsx
+++ src/client/app/components/UI/NavBar.jsx
@@ -1,11 +1,4 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-// eslint-disable-next-line no-unused-vars
-// import { useSelector } from 'react-redux';
-// import NavProfile from './navProfile';
-// import { getCurrentUser } from '../../store/users';
-import { useSelector } from 'react-redux';
-import { getLoginStatus } from '../../store/user';
 import NavProfile from './navProfile';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import showElement from '../../../../server/utils/console/showElement';
@@ -15,7 +8,9 @@ import NavList from './navlist';
 export default function NavBar() {
   return (
     <div className="navbar">
-      <div className="navlist--top"><NavList /></div>
+      <div className="navlist--top">
+        <NavList />
+      </div>
       <div className="user-panel">
         <UserBalance />
         <NavProfile />
